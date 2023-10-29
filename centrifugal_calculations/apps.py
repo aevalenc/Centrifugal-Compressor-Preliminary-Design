e40592b7d35a426ec1c9eed5594a1d46883bbf0f
aevalenc <aevalenc@syr.edu>
2023-10-29 13:19:41 +0100

    Setup centrifugal_calculations application

---
+++ centrifugal_calculations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CentrifugalCalculationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "centrifugal_calculations"
