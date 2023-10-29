cffda65cec981cf04fb8bc6d4d354641b461f773
aevalenc <aevalenc@syr.edu>
2023-10-29 13:47:50 +0100

    Rename centrifugal_calculations to ccpd

--- neptune/neptune/urls.py
+++ neptune/neptune/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("centrifugal_calculations/", include("centrifugal_calculations.urls")),
+    path("ccpd/", include("ccpd.urls")),
     path("admin/", admin.site.urls),
 ]
