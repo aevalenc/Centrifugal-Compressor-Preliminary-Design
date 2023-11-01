2d05242b960bc809fab8bf5901aaa87458a39c19
aevalenc <aevalenc@syr.edu>
2023-11-01 18:19:50 +0100

    Incorporate ccpd_dev:main into neptune

--- neptune/ccpd/urls.py
+++ neptune/ccpd/urls.py
@@ -4,10 +4,12 @@ Update: October 29, 2023
 """
 
 from django.urls import path
-from . import views
+from neptune.ccpd import views
 
 # Here we connect views to urls
 urlpatterns = [
     path("", views.index, name="ccpd-index"),
     path("about/", views.about, name="ccpd-about"),
+    path("run_hello/", views.run_hello_world, name="ccpd-run_hello_world"),
+    path("run_main/", views.run_main, name="ccpd-run-main"),
 ]
