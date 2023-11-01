921181bd0f27ce4c51336fe715665fba3a2ea9f7
aevalenc <aevalenc@syr.edu>
2023-11-01 11:54:22 +0100

    Create html templates for home and about pages

--- neptune/ccpd/views.py
+++ neptune/ccpd/views.py
@@ -5,17 +5,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def index(request) -> HttpResponse:
     """
     pass a request to the index
     """
-    return HttpResponse("Hello, world. Welcome to project neptune: the hub of ccpd.")
+    return render(request, "ccpd/home.html")
 
 
-def about(request):
-    return HttpResponse(
-        """
-        ccpd is a application to generate a preliminary design of a centrifugal compressor.\n
-        The user can input certain design parameters to obtain a desired design
-        """
-    )
+def about(request) -> HttpResponse:
+    return render(request, "ccpd/about.html")
