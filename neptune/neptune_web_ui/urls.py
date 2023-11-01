06890ba128b18d8961c378045b0ccc3f2333c764
aevalenc <aevalenc@syr.edu>
2023-11-01 11:11:45 +0100

    Make index default home landing page

--- neptune/neptune_web_ui/urls.py
+++ neptune/neptune_web_ui/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("ccpd/", include("ccpd.urls")),
+    path("", include("ccpd.urls")),
     path("admin/", admin.site.urls),
 ]
