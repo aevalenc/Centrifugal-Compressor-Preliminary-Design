eb59ac4aa18f111f9490e58c90d3ded8cf9d73bf
aevalenc <aevalenc@syr.edu>
2023-11-01 13:02:59 +0100

    Create base template html files

--- neptune/ccpd/views.py
+++ neptune/ccpd/views.py
@@ -4,13 +4,31 @@ Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
 
+fluids = [
+    {
+        "name": "air",
+        "specific_heat": 1006.0,
+        "specific_ratio": 1.4,
+        "specific_gas_constant": 287.0,
+        "kinematic_viscosity": 18.13e-6,
+    },
+    {
+        "name": "hydrogen",
+        "specific_heat": 14310.0,
+        "specific_ratio": 1.41,
+        "specific_gas_constant": 4120.0,
+        "kinematic_viscosity": 0.88e-5,
+    },
+]
+
 
 def index(request) -> HttpResponse:
     """
-    pass a request to the index
+    pass a request to the index(home page)
     """
-    return render(request, "ccpd/home.html")
+    context = {"fluids": fluids}
+    return render(request, "ccpd/home.html", context)
 
 
 def about(request) -> HttpResponse:
-    return render(request, "ccpd/about.html")
+    return render(request, "ccpd/about.html", {"title": "About"})
