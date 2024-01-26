140b7f3c0ff1af1a6ae9e0dc90390490935e7192
aevalenc <aevalenc@syr.edu>
2024-01-26 01:35:19 +0100

    second bite

--- neptune/manage.py
+++ neptune/manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neptune_web_ui.settings")
+    print("Hello from Neptune!")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
